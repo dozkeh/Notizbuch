699bafde8f5003796a0c2bdee8d424fd4604e6a2
Romina <Romina.kehl@googlemail.com>
2017-03-25 22:59:58 +0100

    Activität mit Ressourcen und Manifest hinzugefügt, um einen Wechsel zu ermöglichen. ItemClick funktioniert

--- app/src/main/java/com/example/rominakehl/notizbuch/DetailViewActivity.java
+++ app/src/main/java/com/example/rominakehl/notizbuch/DetailViewActivity.java
@@ -1,7 +1,17 @@
 package com.example.rominakehl.notizbuch;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by rominakehl on 25.03.17.
  */
-public class DetailViewActivity {
+public class DetailViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_view_activity_layout);
+    }
 }
